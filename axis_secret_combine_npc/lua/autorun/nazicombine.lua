6513bab8e29964e2cd8b9d95426fb1c723e26a46
WhitetubbyModMakerForGmod2141 <88854210+WhitetubbyModMakerForGmod2141@users.noreply.github.com>
2021-08-13 20:07:45 +0200

    Add files via upload

---
+++ axis_secret_combine_npc/lua/autorun/nazicombine.lua
@@ -0,0 +1,10 @@
+local Category = "Combine"
+
+local NPC = {   Name = "Nazi Combine", 
+                Class = "npc_combine",
+                Model = "models/combine_soldier_nazi.mdl",
+                Health = "100", 
+                Weapons = { "weapons_smg1" }, 
+                Category = Category }
+                               
+list.Set( "NPC", "npc_nazi_combine", NPC )
---
+++ axis_secret_combine_npc/lua/autorun/nazielitecombine.lua
@@ -0,0 +1,10 @@
+local Category = "Combine"
+
+local NPC = {   Name = "Nazi Elite Combine", 
+                Class = "npc_combine",
+                Model = "models/Combine_Elite_nazi.mdl",
+                Health = "100", 
+                Weapons = { "weapons_smg1" }, 
+                Category = Category }
+                               
+list.Set( "NPC", "npc_nazielite_combine", NPC )
